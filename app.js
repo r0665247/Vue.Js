f687e03a64aecb387d098d2cd25de86824a13141
Quentin Tilman <r0665247@student.vives.be>
2019-06-09 18:02:07 +0200

    show/complete options

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 Vue.component("messages", {
-    template: "<div style='border: solid; margin-top: 20px; width:400px;' v-show='isVisible'><h4>Message</h4> <p><slot></slot></p> <button @click='isVisible = false;'>hide</button></div>",
+    template: "<div style='border: solid; margin-top: 20px; width:400px;' v-show='isVisible'><h4>Message</h4> <p><slot></slot></p> <button @click=\"$emit('delete')\">Delete</button> <button @click='isVisible = false;'>hide</button></div>",
     data(){
         return {
             isVisible : true
@@ -15,6 +15,8 @@ new Vue({
         message: "",
         disabled: false,
         messages : [],
+        showMsg : false,
+        aantalMsg : "",
 
     },
     methods: {
@@ -22,17 +24,42 @@ new Vue({
             console.log("activate");
             this.disabled = false;
         },
+        sumbit() {
+            this.messages.push({task :this.message, complete : false});
+            this.aantalMsg = this.messages.length;
+            this.disabled = true;
+            this.message = "";
+            setTimeout(this.active, 1000);
+        },
         complete(message){
             let index = this.messages.indexOf(message);
             console.log(index);
-            this.messages[index] = {task : message.task , complete : true}
+            this.messages[index] = {task : message.task , complete : true};
+            console.log(this.messages[index]);
+            this.refresh();
         },
-        sumbit() {
-            this.messages.push({task :this.message, complete : false});
-            this.disabled = true;
-            this.message = "";
-            setTimeout(this.active, 5000);
+        showMessages()
+        {
+            if(this.showMsg === true)
+            {
+                this.showMsg = false;
+            }
+            else
+            {
+                this.showMsg = true;
+            }
+            
+            console.log(this.showMsg);
         },
+        delete(message)
+        {
+
+        },
+        refresh()
+        {
+            this.showMsg = false;
+            this.showMsg = true;
+        }
 
     },
 })
